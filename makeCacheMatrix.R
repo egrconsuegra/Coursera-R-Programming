6ed2d096fc8a6cc189f8b850b08424c1b0ea2013
egrconsuegra <82446432+egrconsuegra@users.noreply.github.com>
2021-04-22 15:12:22 +0800

    Update makeCacheMatrix.R

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,23 +1,36 @@
-makeCacheMatrix <- function(x = matrix()){
+## CACHING THE INVERSE OF A MATRIX
+## In this programming assignment, caching the inverse of a matrix
+## using the makeCacheMatrix and cacheSolve functions are practiced.
+
+## The first function, makeCacheMatrix, is a function that assigns a special "vector" or "matrix"
+## which could set and get the value of the vector as a variable and also its inverse.
+
+## With regards to the function used in caching the mean of a vector, all "mean" functions
+## from the sample code were changed to "inv" functions to indicate the inverse of the given vector.
+
+makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
-  set <- function(y){
+  set <- function(y) {
     x <<- y
     inv <<- NULL
   }
-  get <- function() {x}
-  setInverse <- function(inverse) {inv <<- inverse}
-  getInverse <- function() {inv}
-  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+  get <- function() x
+  setinv <- function(inv_a) inv <<- inv_a
+  getinc <- function() inv
+  list(set = set, get = get,
+       setinv = setinv, getinv = getinv)
 }
 
-cacheSolve <- function(x, ...){
-  inv <- x$getInverse()
-  if(!is.null(inv)){
-    message("getting cached data")
+## Solving for the inverse of the special "vector" or "matrix" using the function below.
+
+cacheSolve <- function(x, ...) {
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("Getting the Inveresd Matrix")
     return(inv)
   }
-  mat <- x$get()
-  inv <- solbe(mat, ...)
-  x$setInverse(inv)
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$etinv(inv)
   inv
 }
